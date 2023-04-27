aebdcb8abacb92cd31deefc364487b4b96bcd615
Pasha <pkolinko@newage.io>
2023-04-27 18:33:48 +0300

    Fixed another

--- clients/entities/User.js
+++ clients/entities/User.js
@@ -90,8 +90,8 @@ class User {
       console.log(
         `Single device of user with name ${this.name} and id ${uuid}`
       );
-      const body = { userId: this.id, deviceId: uuid };
-      const { data } = await this.apiClient.post(
+      const body = { userID: this.id, deviceID: uuid };
+      const { data } = await this.apiClient.get(
         `api/v1/deviceToUser/getDeviceById`,
         body
       );
